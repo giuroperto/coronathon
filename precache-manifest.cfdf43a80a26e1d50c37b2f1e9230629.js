ef755d38ac15f2997254467b5ae95aca7cb5651c
Giu Roperto <giu.roperto@gmail.com>
2020-06-14 18:29:32 -0300

    Updates

---
+++ precache-manifest.cfdf43a80a26e1d50c37b2f1e9230629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5da1020bd71ef1e8793ee2fea08887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b3242e43ab8d6b6b19",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "502d62ec1df833038e9e",
+    "url": "/static/css/main.8c444a9c.chunk.css"
+  },
+  {
+    "revision": "73b3242e43ab8d6b6b19",
+    "url": "/static/js/2.f75764ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f75764ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502d62ec1df833038e9e",
+    "url": "/static/js/main.fe136efd.chunk.js"
+  },
+  {
+    "revision": "8d68fd0a9b971ab65f9e",
+    "url": "/static/js/runtime-main.1db2b5ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronathon/precache-manifest.fc8e241c6961be34ed9d89552a65cfdd.js"
+  "/precache-manifest.cfdf43a80a26e1d50c37b2f1e9230629.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coronathon/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
