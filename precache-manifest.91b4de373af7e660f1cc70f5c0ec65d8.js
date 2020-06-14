e7fed0664a3f2571e92361ffcf23e1833a7bb148
Giu Roperto <giu.roperto@gmail.com>
2020-06-14 08:22:12 -0300

    Updates

---
+++ precache-manifest.91b4de373af7e660f1cc70f5c0ec65d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8aac02742332624bde919156a720460",
+    "url": "/coronathon/index.html"
+  },
+  {
+    "revision": "73b3242e43ab8d6b6b19",
+    "url": "/coronathon/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0989299f396a8efe7d59",
+    "url": "/coronathon/static/css/main.26e28f45.chunk.css"
+  },
+  {
+    "revision": "73b3242e43ab8d6b6b19",
+    "url": "/coronathon/static/js/2.f75764ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coronathon/static/js/2.f75764ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0989299f396a8efe7d59",
+    "url": "/coronathon/static/js/main.80a039e0.chunk.js"
+  },
+  {
+    "revision": "af68926e78fbe989ecf8",
+    "url": "/coronathon/static/js/runtime-main.5a9da991.js"
+  }
+]);
