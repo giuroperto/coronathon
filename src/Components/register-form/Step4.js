abf346274f7e630e6d7f83ee5ae65adfa879e40d
Giu Roperto <giu.roperto@gmail.com>
2020-06-14 21:39:35 -0300

    last page form done

--- src/Components/register-form/Step4.js
+++ src/Components/register-form/Step4.js
@@ -14,139 +14,118 @@ class Step4 extends Component {
       <>
       <div className="form-group all-sections d-flex flex-column align-items-center">
         <div className='d-flex mb-3'>
-          <div className='d-flex flex-column mr-4'>
-            <label className='form-field' htmlFor="name">Nome Completo*</label>
-            <input
-              className="form-control"
-              id="name"
-              name="name"
-              type="text"
-              required
-            />
-          </div>
-          <div className='d-flex flex-column mr-4'>
-            <label className='form-field'  htmlFor="birth">Data de Nascimento*</label>
-            <input
-              className="form-control"
-              id="birth"
-              name="birth"
-              type="date"
-              required
-            />
-          </div>
-          <div className='d-flex flex-column mr-4'>
-            <label className='form-field'  htmlFor="cpf">CPF*</label>
-            <input
-              className="form-control"
-              id="cpf"
-              name="cpf"
-              type="number"
-              required
-            />
-          </div>
-          <div className='d-flex flex-column'>
-            <label className='form-field'  htmlFor="cpf">RG*</label>
-            <input
-              className="form-control"
-              id="rg"
-              name="rg"
-              type="number"
-              required
-            />
-          </div>
-        </div>
-        <div className='d-flex mb-3'>
-          <div className='d-flex flex-column mr-4'>
-            <label className='form-field'  htmlFor="email">E-mail*</label>
-            <input
-              className="form-control"
-              id="email"
-              name="email"
-              type="email"
-              required
-            />
-          </div>
-          <div className='d-flex flex-column mr-4'>
-            <label className='form-field'  htmlFor="tel">Telefone</label>
-            <input
-              className="form-control"
-              id="tel"
-              name="tel"
-              type="tel"
-            />
-          </div>
-          <div className='d-flex flex-column'>
-            <label className='form-field'  htmlFor="impairment">Possui deficiência*</label>
-            <input
-              className="form-control"
-              id="impairment"
-              name="impairment"
-              type="impairment"
-              required
-            />
-          </div>
-        </div>
-        <div className='d-flex mb-3'>
-          <div className='d-flex flex-column mr-4'>
-            <label className='form-field'  htmlFor="cep">CEP*</label>
-            <input
-              className="form-control"
-              id="cep"
-              name="cep"
-              type="number"
-              required
-            />
-          </div>
-          <div className='d-flex flex-column mr-4'>
-            <label className='form-field'  htmlFor="address">Endereço*</label>
-            <input
-              className="form-control"
-              id="address"
-              name="address"
-              type="text"
-              required
-            />
-          </div>
-          <div className='d-flex flex-column'>
-            <label className='form-field'  htmlFor="complemento">Complemento</label>
-            <input
-              className="form-control"
-              id="complemento"
-              name="complemento"
-              type="text"
-            />
-          </div>
-        </div>
-        <div className='d-flex'>
-          <div className='d-flex flex-column mr-4'>
-            <label className='form-field'  htmlFor="nationality">Nacionalidade*</label>
-            <input
-              className="form-control"
-              id="nationality"
-              name="nationality"
-              type="text"
-              required
-            />
-          </div>
-          <div className='d-flex flex-column mr-4'>
-            <label className='form-field'  htmlFor="state">Estado*</label>
-            <input
-              className="form-control"
-              id="state"
-              name="state"
-              type="text"
-              required
-            />
-          </div>
-          <div className='d-flex flex-column'>
-            <label className='form-field'  htmlFor="city">Cidade*</label>
-            <input
-              className="form-control"
-              id="city"
-              name="city"
-              type="text"
-              required
-            />
+            <div className='d-flex flex-column mr-4'>
+              <label className='form-field'  htmlFor="habilities">Habilidades*</label>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="analitical"
+                  name="analitical"
+                  type="checkbox"
+                  value="analitical"
+                  required
+                />
+                <label htmlFor="analitical" className='ml-2'>Capacidade Analítica</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="negotiation"
+                  name="negotiation"
+                  type="checkbox"
+                  value="negotiation"
+                  required
+                />
+                <label htmlFor="negotiation" className='ml-2'>Negociação</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="team"
+                  name="team"
+                  type="checkbox"
+                  value="team"
+                  required
+                />
+                <label htmlFor="team" className='ml-2'>Trabalho em Equipe</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="leadership"
+                  name="leadership"
+                  type="checkbox"
+                  value="leadership"
+                  required
+                />
+                <label htmlFor="leadership" className='ml-2'>Liderança</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="organization"
+                  name="organization"
+                  type="checkbox"
+                  value="organization"
+                  required
+                />
+                <label htmlFor="organization" className='ml-2'>Organização</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="flexibility"
+                  name="flexibility"
+                  type="checkbox"
+                  value="flexibility"
+                  required
+                />
+                <label htmlFor="flexibility" className='ml-2'>Flexibilidade</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="criativity"
+                  name="criativity"
+                  type="checkbox"
+                  value="criativity"
+                  required
+                />
+                <label htmlFor="criativity" className='ml-2'>Criatividade</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="planning"
+                  name="planning"
+                  type="checkbox"
+                  value="planning"
+                  required
+                />
+                <label htmlFor="planning" className='ml-2'>Planejamento</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="proactivity"
+                  name="proactivity"
+                  type="checkbox"
+                  value="proactivity"
+                  required
+                />
+                <label htmlFor="proactivity" className='ml-2'>Proatividade</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="produtivity"
+                  name="produtivity"
+                  type="checkbox"
+                  value="produtivity"
+                  required
+                />
+                <label htmlFor="produtivity" className='ml-2'>Produtividade</label>
+              </div>
+              <div className="form-options d-flex justify-content-start align-items-baseline">
+                <input
+                  id="empathy"
+                  name="empathy"
+                  type="checkbox"
+                  value="empathy"
+                  required
+                />
+                <label htmlFor="empathy" className='ml-2'>Empatia</label>
+            </div>
           </div>
         </div>
         <button className='btn btn-style mt-5'>SALVAR</button>
